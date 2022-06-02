129dab8ca9c0165105902982dee7b95e0778ba22
Alisson Andre <alissonandresb@gmail.com>
2022-06-02 09:15:25 -0300

    Method reference static

--- courseLambda2/src/application/Program.java
+++ courseLambda2/src/application/Program.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entities.Product;
-import util.ProductPredicate;
 
 public class Program {
 
@@ -18,7 +17,8 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 		
 		//list.removeIf(p -> p.getPrice() >= 100);
-		list.removeIf(new ProductPredicate());
+		//list.removeIf(new ProductPredicate());
+		list.removeIf(Product :: staticProductPredicate);
 		
 		for (Product p : list) { 			
 			System.out.println(p);
